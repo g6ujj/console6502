b8a76366594659386475640db2aa0d65826caf4d
ns <ns@ns-VirtualBox>
2011-10-16 20:42:40 +0100

    Initial add of test routines.

---
+++ main.cpp
@@ -0,0 +1,53 @@
+//
+//  main.cpp
+//
+//  Author:
+//       Neil Stoker <gg6ujj@gmail.com>
+//
+//  Copyright (c) 2011 Neil Stoker
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "main.h"
+#include "versionConsole.h"
+
+
+int main()
+{
+    cout << "6502 Simulator. Initialising..." << endl;
+    string s=AutoVersion::FULLVERSION_STRING;
+    cout<<"Build   :"<<s<<endl;
+    cout<<"Library :"<<Lib6502Version::FULLVERSION_STRING<<endl;
+    ram=new Memory();
+    cpu=new CPU(ram);
+
+    cpuTest();
+
+    cout<<"6502 Simulator. Exiting..."<<endl;
+    return 0;
+}
+
+void cpuTest()
+{
+    cout<<"CPU Testing..."<<endl;
+    cpuTest001();
+    cout<<endl<<"... all tests completed."<<endl<<endl;
+}
+void cpuTest001()
+{
+    //
+}
---
+++ versionConsole.h
@@ -0,0 +1,33 @@
+#ifndef CVERSION_H
+#define CVERSION_H
+
+namespace AutoVersion{
+
+	//Date Version Types
+	static const char DATE[] = "12";
+	static const char MONTH[] = "10";
+	static const char YEAR[] = "2011";
+	static const char UBUNTU_VERSION_STYLE[] = "11.10";
+
+	//Software Status
+	static const char STATUS[] = "Alpha";
+	static const char STATUS_SHORT[] = "a";
+
+	//Standard Version Type
+	static const long MAJOR = 0;
+	static const long MINOR = 0;
+	static const long BUILD = 1;
+	static const long REVISION = 3;
+
+	//Miscellaneous Version Types
+	static const long BUILDS_COUNT = 22;
+//	#define RC_FILEVERSION 0,0,1,3
+//	#define RC_FILEVERSION_STRING "0, 0, 1, 3\0"
+	static const char FULLVERSION_STRING[] = "0.0.1.3";
+
+	//These values are to keep track of your versioning state, don't modify them.
+	static const long BUILD_HISTORY = 1;
+
+
+}
+#endif //CVERSION_H
